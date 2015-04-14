088b2de4d57ffec535ce4d291098100a0e3391d3
Lars Mescheder <larsmescheder@gmx.net>
2015-04-14 14:04:30 +0200

    Do some work on hull tracking.

--- stabilizer/MatMserTracker.cpp
+++ stabilizer/MatMserTracker.cpp
@@ -91,7 +91,7 @@ void MatMserTracker::track_msers_(std::vector<MatMserTracker::ComponentStats> &m
 
     cv::Mat err, status;
     points1.reserve(points0.size());
-    cv::calcOpticalFlowPyrLK(last_image_, new_image, points0, points1, status, err, lk_window_);
+    cv::calcOpticalFlowPyrLK(last_image_, new_image, points0, points1, status, err, lk_window_, 3);
 
     assert(points0.size() == points1.size());
     assert(msers.size() == hull_Ns.size());
@@ -99,17 +99,92 @@ void MatMserTracker::track_msers_(std::vector<MatMserTracker::ComponentStats> &m
     int j1 = 0;
     int j2 = 0;
     for (std::size_t i=0; i<msers.size(); ++i) {
-        int N = hull_Ns[i];
-        j1 = j2;
-        j2 += N;
+        if (hull_Ns[i] == 0)
+            continue;
 
-        cv::Point2f t{0.f, 0.f};
-
-        for (int j=j1; j<j2; ++j)
-            t += (points1[j] - points0[j])/N;
-        // update mser
         auto& mser = msers[i];
-        mser.mean += t;
+        j1 = j2;
+        j2 += hull_Ns[i];
+
+        // get points
+        std::vector<cv::Point2f> mser_points0, mser_points1;
+        mser_points0.reserve(hull_Ns[i]);
+        mser_points1.reserve(hull_Ns[i]);
+
+        for (int j=j1; j<j2; ++j) {
+            if (status.at<bool>(j)) {
+                mser_points0.push_back(points0[j]);
+                mser_points1.push_back(points1[j]);
+            }
+        }
+
+        if (mser_points0.size() < 3) {
+            mser.N = 0;
+            continue;
+        }
+
+        // estimate affine transformation
+        cv::Mat_<float> A = cv::estimateRigidTransform(mser_points0, mser_points1, true);
+
+
+        // update mser
+
+
+        if (A.empty()) {
+            mser.N = 0;
+            continue;
+        }
+
+        cv::Point2f new_mean;
+        cv::Matx22f new_cov;
+        cv::Point2i new_source;
+        cv::Point2i new_min_point;
+        cv::Point2i new_max_point;
+        int new_N;
+
+        new_mean.x = A(0, 0) * mser.mean.x + A(0, 1) * mser.mean.y + A(0, 2);
+        new_mean.y = A(1, 0) * mser.mean.x + A(1, 1) * mser.mean.y + A(1, 2);
+
+        for (int idx1 : {0, 1}) for (int idx2 : {0, 1})
+        for (int idx3 : {0, 1}) for (int idx4 : {0, 1})
+            new_cov(idx1, idx3) += A(idx1, idx2) * A(idx3, idx4) * mser.cov(idx2, idx4);
+
+        new_source.x = static_cast<int>(A(0, 0) * mser.source.x + A(0, 1) * mser.source.y + A(0, 2));
+        new_source.y = static_cast<int>(A(1, 0) * mser.source.x + A(1, 1) * mser.source.y + A(1, 2));
+
+        cv::Point2i new_corner11, new_corner12, new_corner21, new_corner22;
+
+        new_corner11.x = static_cast<int>(A(0, 0) * mser.min_point.x + A(0, 1) * mser.min_point.y + A(0, 2));
+        new_corner11.y = static_cast<int>(A(1, 0) * mser.min_point.x + A(1, 1) * mser.min_point.y + A(1, 2));
+
+        new_corner12.x = static_cast<int>(A(0, 0) * mser.min_point.x + A(0, 1) * mser.max_point.y + A(0, 2));
+        new_corner12.y = static_cast<int>(A(1, 0) * mser.min_point.x + A(1, 1) * mser.max_point.y + A(1, 2));
+
+        new_corner21.x = static_cast<int>(A(0, 0) * mser.max_point.x + A(0, 1) * mser.min_point.y + A(0, 2));
+        new_corner21.y = static_cast<int>(A(1, 0) * mser.max_point.x + A(1, 1) * mser.min_point.y + A(1, 2));
+
+        new_corner22.x = static_cast<int>(A(0, 0) * mser.max_point.x + A(0, 1) * mser.max_point.y + A(0, 2));
+        new_corner22.y = static_cast<int>(A(1, 0) * mser.max_point.x + A(1, 1) * mser.max_point.y + A(1, 2));
+
+        new_min_point.x = std::min({new_corner11.x, new_corner12.x, new_corner21.x, new_corner22.x});
+        new_min_point.y = std::min({new_corner11.y, new_corner12.y, new_corner21.y, new_corner22.y});
+
+        new_max_point.x = std::max({new_corner11.x, new_corner12.x, new_corner21.x, new_corner22.x});
+        new_max_point.y = std::max({new_corner11.y, new_corner12.y, new_corner21.y, new_corner22.y});
+
+        new_N = static_cast<int> ((A(0, 0) * A(1, 1) - A(0, 1) * A(1, 0)) * mser.N);
+
+        mser.mean = new_mean;
+        mser.cov = new_cov;
+        mser.source = new_source;
+        mser.min_point = new_min_point;
+        mser.max_point = new_max_point;
+        mser.N = new_N;
     }
 
+
 }
+
+
+
+
