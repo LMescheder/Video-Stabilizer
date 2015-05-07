8efda2704fd41c611fd3b5c9f79a7f094d1fa142
Lars Mescheder <larsmescheder@gmx.net>
2015-05-07 13:58:13 +0200

    Take geometric trust.

--- alt_stabilizer/stabilizer.cpp
+++ alt_stabilizer/stabilizer.cpp
@@ -6,7 +6,7 @@ Stabilizer::Stabilizer(const cv::Mat& frame0) {
     cv::goodFeaturesToTrack(frame_gray_0_, points0_, 1000, .01, 8);
     points_ = points0_;
     status_.resize(points0_.size());
-    trust_.resize(points0_.size(), 1.);
+    trust_.resize(points0_.size(), .5);
     last_frame_gray_ = frame_gray_0_;
     H_ = cv::Mat::eye(3, 3, CV_64F);
 }
@@ -19,7 +19,7 @@ cv::Mat Stabilizer::stabilize_next(const cv::Mat& frame) {
 
     std::vector<cv::Point2f> new_points;
 
-    new_points = checked_optical_flow_(H_frame_gray, 1.);
+    new_points = checked_optical_flow_(H_frame_gray, .1);
 
     std::vector<cv::Point2f> good_points0;
     std::vector<cv::Point2f> good_new_points;
@@ -59,8 +59,8 @@ std::vector<cv::Point2f> Stabilizer::checked_optical_flow_(const cv::Mat& frame_
     for (std::size_t i=0; i<points_.size(); ++i) {
         status_[i] = (status1.at<bool>(i) && status2.at<bool>(i) && cv::norm(points0_[i] - points2[i]) < eps );
         float new_trust = status_[i] ? 1.f : 0.f;
-        trust_[i] = (count_*trust_[i] + new_trust)/(count_+1);
-        //trust_[i] = .99 * trust_[i] + .01 * new_trust;
+        //trust_[i] = (count_*trust_[i] + new_trust)/(count_+1);
+        trust_[i] = .95 * trust_[i] + .05 * new_trust;
     }
 
     return points1;
