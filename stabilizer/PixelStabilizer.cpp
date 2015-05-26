5ba2b3b668c618a4e44a9acf2d0c14e2123f7173
Lars Mescheder <larsmescheder@gmx.net>
2015-05-26 17:41:32 +0200

    Write some code for pixel based stabilizer.

--- stabilizer/PixelStabilizer.cpp
+++ stabilizer/PixelStabilizer.cpp
@@ -1,7 +1,44 @@
 #include "PixelStabilizer.h"
 
-PixelStabilizer::PixelStabilizer()
+PixelStabilizer::PixelStabilizer(const cv::Mat& frame_0, Warping warping, Mode mode)
+  : Stabilizer(frame_0, warping, mode, true)
 {
 
+
+}
+
+cv::Mat PixelStabilizer::get_next_homography(const cv::Mat &next_frame)
+{
+
+}
+
+cv::Mat PixelStabilizer::get_b(cv::Mat frame)
+{
+
+  cv::Mat b;
+
+  int width = ref_frame_gray_.cols;
+  int height = ref_frame_gray_.rows;
+
+}
+
+cv::Mat PixelStabilizer::get_A(cv::Mat frame)
+{
+  int width = ref_frame_gray_.cols;
+  int height = ref_frame_gray_.rows;
+
+  // compute derivative of reference image
+  cv::Mat ref_frame_gray_blurred;
+  cv::Mat gradIx, gradIy;
+  cv::GaussianBlur(ref_frame_gray_, ref_frame_gray_blurred, cv::Size(5, 5), 1.);
+  cv::Sobel(ref_frame_gray_blurred, gradIx, 1, 1, 0);
+  cv::Sobol(ref_frame_gray_blurred, gradIy, 1, 0, 1);
+
+  // compute A
+  for (int i=0; i<width; ++i) {
+    for (int j=0; j<height; ++j) {
+      // to do compute contribution to A
+    }
+  }
 }
 
--- stabilizer/PixelStabilizer.h
+++ stabilizer/PixelStabilizer.h
@@ -4,15 +4,22 @@
 #include "opencv2/opencv.hpp"
 #include "Stabilizer.h"
 
-class PixelStabilizer
+class PixelStabilizer : public Stabilizer
 {
 public:
-  PixelStabilizer();
+  PixelStabilizer(const cv::Mat &frame_0, Warping warping, Mode mode);
 
 
 protected:
-  virtual cv::Mat get_next_homography(const cv::Mat& next_frame) = 0;
-  virtual void create_visualization() = 0;
+  virtual cv::Mat get_next_homography(const cv::Mat& next_frame);
+  virtual void create_visualization();
+
+ private:
+  cv::Mat A_, b_;
+
+private:
+  cv::Mat get_b (cv::Mat frame);
+  cv::Mat get_A (cv::Mat frame);
 };
 
 #endif // PIXELSTABILIZER_H
