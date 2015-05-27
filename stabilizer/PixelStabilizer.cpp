d9573a7e8223e46d715c2489abc5fceeb3881f7e
Lars Mescheder <larsmescheder@gmx.net>
2015-05-27 09:20:25 +0200

    First working version of pixel stabilizer.

--- stabilizer/PixelStabilizer.cpp
+++ stabilizer/PixelStabilizer.cpp
@@ -1,29 +1,37 @@
 #include "PixelStabilizer.h"
 
 PixelStabilizer::PixelStabilizer(const cv::Mat& frame_0, Warping warping)
-    : Stabilizer(frame_0, warping, Mode::DIRECT, true)
+    : Stabilizer(frame_0, warping, Mode::WARP_BACK, true)
 {
     init(ref_frame_gray_);
 }
 
 cv::Mat PixelStabilizer::get_next_homography(const cv::Mat &next_frame)
 {
-    int width = ref_frame_gray_.cols;
-    int height = ref_frame_gray_.rows;
+    size_t width = ref_frame_gray_.cols;
+    size_t height = ref_frame_gray_.rows;
 
-    cv::Mat error = next_frame - ref_frame_gray_;
-    Vec8f b = 0;
+    cv::Mat H = cv::Mat::eye(3, 3, CV_64F);
+    cv::Mat frame = next_frame.clone();
 
-    // accumulate b
-    for (size_t i = 0; i < height; ++i)
-        for (size_t j = 0; j < width; ++j)
-            b += error.at<uchar>(i, j) * Ji_.at<Vec8f>(i, j);
+    for (int i = 0; i < 40; ++ i) {
+        cv::Mat error = frame - ref_frame_gray_;
+        Vec8f b = 0;
 
-    Vec8f h = Ainv_ * b;
+        // accumulate b
+        for (size_t i = 0; i < height; ++i)
+            for (size_t j = 0; j < width; ++j) {
+                b -= static_cast<float>(error.at<uchar>(i, j)) * Ji_.at<Vec8f>(i, j);
+            }
+        Vec8f h = Ainv_ * b;
 
-    cv::Mat H = (cv::Mat_<double>(3, 3) << 1 + h(0), h(1), h(2),
-                                          h(3), 1 + h(4), h(5),
-                                          h(6), h(7), 1.f );
+        cv::Mat dH = (cv::Mat_<double>(3, 3) << 1 + h(0), h(1), h(2),
+                                              h(3), 1 + h(4), h(5),
+                                              h(6), h(7), 1.f );
+
+        H = H * dH;
+        cv::warpPerspective(next_frame, frame, H.inv(), cv::Size(width, height));
+    }
 
     return H.inv();
 
@@ -35,34 +43,35 @@ void PixelStabilizer::create_visualization()
 
 void PixelStabilizer::init(const cv::Mat& frame)
 {
-    int width = ref_frame_gray_.cols;
-    int height = ref_frame_gray_.rows;
+    int width = frame.cols;
+    int height = frame.rows;
 
     // compute derivative of reference image
-    cv::Mat ref_frame_gray_blurred;
+    cv::Mat frame_blurred, frame_blurred0;
     cv::Mat gradIx, gradIy;
-    cv::GaussianBlur(ref_frame_gray_, ref_frame_gray_blurred, cv::Size(5, 5), .01);
-    ref_frame_gray_blurred.convertTo(ref_frame_gray_blurred, CV_32F);
-    cv::Sobel(ref_frame_gray_blurred, gradIx, CV_32F, 1, 0);
-    cv::Sobel(ref_frame_gray_blurred, gradIy, CV_32F, 0, 1);
+    cv::GaussianBlur(frame, frame_blurred0, cv::Size(5, 5), 1.);
+    frame_blurred0.convertTo(frame_blurred, CV_32F);
+    cv::Sobel(frame_blurred, gradIx, CV_32F, 1, 0);
+    cv::Sobel(frame_blurred, gradIy, CV_32F, 0, 1);
 
     // compute A
+    float one_over_N = 1./(width * height);
     Matx88f A = 0;
     Ji_ = cv::Mat::zeros(height, width, CV_32FC(8));
-    int N = width * height;
     for (int i=0; i<height; ++i) {
         for (int j=0; j<width; ++j) {
             float x = static_cast<float> (j);
             float y = static_cast<float> (i);
             cv::Matx<float, 2, 8> Jx = {x,   y,   1.f, 0.f, 0.f, 0.f, -x*x, -x*y,
                                         0.f, 0.f, 0.f, x,   y,   1.f, -x*y, -y*y};
-            cv::Vec2f gradI (256 * gradIx.at<float>(i, j), 256 * gradIy.at<float>(i, j));
+            cv::Vec2f gradI (gradIx.at<float>(i, j), gradIy.at<float>(i, j));
 
-            A += Jx.t() * gradI * gradI.t() * Jx;
-            Ji_.at<Vec8f>(i, j) =  Jx.t() * gradI;
+            A += one_over_N * Jx.t() * gradI * gradI.t() * Jx;
+            Ji_.at<Vec8f>(i, j) =  one_over_N * Jx.t() * gradI;
         }
     }
 
     Ainv_ = A.inv();
+    std::cout << Ainv_ << std::endl;
 }
 
